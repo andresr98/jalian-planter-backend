11fc0b592e8de5ffaaff6bca270ee2779fc67806
Andres Ruiz Graciano <aandres.r98@gmail.com>
2020-05-27 11:39:22 -0500

    initial commit

---
+++ src/test/java/com/jalian/planter/PlanterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalian.planter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
